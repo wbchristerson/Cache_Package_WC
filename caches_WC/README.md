a2f7a4b291f1e0c6096aa56dac38ff211dff57dd
Will Christerson <wbchristerson@gmail.com>
2019-11-22 21:34:25 -0500

    docs: Begin adding documentation for purpose of package

---
+++ caches_WC/README.md
@@ -0,0 +1,4 @@
+# caches_WC package
+
+This package provides basic LRU (least recently used) cache and LFU (least
+frequently used) cache implementations. 
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='caches_WC',
+      version='0.1',
+      description='LRU cache and LFU cache implementations',
+      packages=['caches_WC'],
+      zip_safe=False)
